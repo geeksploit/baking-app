17b22510ce08e1e4fd45d80635968b33bdcf917a
Max Rem <max@geeksploit.me>
2018-07-30 14:03:40 +0300

    feat: Add step intity

---
+++ app/src/main/java/me/geeksploit/bakingapp/data/StepEntity.java
@@ -0,0 +1,32 @@
+package me.geeksploit.bakingapp.data;
+
+import java.io.Serializable;
+
+public final class StepEntity implements Serializable {
+
+    private int id;
+    private String shortDescription;
+    private String description;
+    private String videoURL;
+    private String thumbnailURL;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+}
