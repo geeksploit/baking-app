08f7ee006e4bc229884cd5b51a0e7bb7a69af5c6
Max Rem <max@geeksploit.me>
2018-07-30 19:07:32 +0300

    feat: Employ RecyclerView to display recipes
    Resolves: #3

--- app/src/main/java/me/geeksploit/bakingapp/MainActivity.java
+++ app/src/main/java/me/geeksploit/bakingapp/MainActivity.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.geeksploit.bakingapp.adapter.RecipeGalleryAdapter;
+import me.geeksploit.bakingapp.data.RecipeEntity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecipeGalleryAdapter mRecipeGalleryAdapter;
+    private List<RecipeEntity> mRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +34,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mRecipes = new ArrayList<>();
+        mRecipeGalleryAdapter = new RecipeGalleryAdapter(this, mRecipes);
+        RecyclerView recipeGallery = findViewById(R.id.recipe_gallery);
+        recipeGallery.setAdapter(mRecipeGalleryAdapter);
     }
 }
