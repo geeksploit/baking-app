d10a3f1a403a65a21ebe36a847f1524bbb239621
Max Rem <max@geeksploit.me>
2018-07-31 12:58:02 +0300

    feat: Add widget gallery items factory stub

---
+++ app/src/main/java/me/geeksploit/bakingapp/widget/GridRemoteViewsFactory.java
@@ -0,0 +1,52 @@
+package me.geeksploit.bakingapp.widget;
+
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+public final class GridRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public RemoteViews getViewAt(int i) {
+        return null;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+}
